03630eef39e6bcd9c2ad70a948fa356943c46ab4
rdhss <ridhosuhendar1@gmail.com>
2022-09-17 09:34:53 +0700

    button

---
+++ src/components/Atom/ButtonSubmit.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button class="px-4 py-2 bg-blue-500" @click="submit">{{ label }}</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "ButtonSubmit",
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    submit() {
+      this.$emit("onsubmit", true);
+    },
+  },
+};
+</script>
---
+++ src/components/Atom/Textfield.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <input type="text" v-model="changeInput" />
+  </div>
+</template>
+<script>
+export default {
+  name: "TextField",
+  data() {
+    return {
+      value: "",
+    };
+  },
+  computed: {
+    changeInput: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        this.$emit("change", val);
+      },
+    },
+  },
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
