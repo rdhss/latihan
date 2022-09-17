d1472f7708fc1e706f0ccacdab8761c766f136d5
unknown <hazvinurafgan@gmail.com>
2022-09-18 06:20:19 +0700

    add pkg tailwind, daisyui, and other. then clean code directory routes, also delete id #app style from home and add fitur navbar

--- src/pages/Posting.vue
+++ src/pages/Posting.vue
@@ -1,11 +1,15 @@
 <template>
     <main>
+        <Navbar />
         <p>Posting Pages</p>
     </main>
 </template>
 
 <script>
+    import Navbar from '@/components/molecules/Navbar.vue';
+    
     export default {
-        name:'PostingPages'
-    }
+    name: "PostingPage",
+    components: { Navbar }
+}
 </script>
