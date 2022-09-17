bbb1a94922c8992ff66b8f1af67cb7d9a80615e9
rdhss <ridhosuhendar1@gmail.com>
2022-09-17 13:11:13 +0700

    develop

---
+++ src/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>HOME</h1>
+</template>
+  
+  <script>
+  
+  export default {
+    name: 'HomePage',
+  }
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+  
---
+++ src/pages/Login.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        Logins
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'HomePage',
+  }
+</script>
